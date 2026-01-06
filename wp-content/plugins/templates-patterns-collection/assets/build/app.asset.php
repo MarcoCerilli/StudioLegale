eb6ff28dec69b127a7cf689296f48c324fa13976
Marco Cerilli <cerillimarco15@gmail.com>
2026-01-06 22:32:21 +0100

    Rimozione definitiva file WordPress e pulizia repository

--- wp-content/plugins/templates-patterns-collection/assets/build/app.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-primitives'), 'version' => 'd2c873d387c2a272fd25');
