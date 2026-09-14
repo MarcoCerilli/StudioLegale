654b1ca4e1f0eaa3502dc628f26ad71168208ac0
Marco Cerilli <cerillimarco15@gmail.com>
2026-09-14 11:48:06 +0200

    fix(seo): update canonical domain to www.avvocatoannafusco.it for search console sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://avvocatoannafusco.it',
+  site: 'https://www.avvocatoannafusco.it',
   vite: {
     plugins: [tailwindcss()]
   },
